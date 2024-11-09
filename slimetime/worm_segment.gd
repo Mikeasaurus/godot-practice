7323b57dbccc0f3f7dd3eb5ed711f339f8d33007
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2024-11-09 16:21:46 -0500

    Smoother animation of realignment of segment angle, and make target gravity point less jumpy when momentarily drifting off a surface.

--- slimetime/worm_segment.gd
+++ slimetime/worm_segment.gd
@@ -2,14 +2,13 @@ extends RigidBody2D
 
 class_name WormSegment
 
-# How fast the worm will walk on a surface (pixels/second)
-@export var walking_speed: float = 100.0
-
 # Attraction point.  Either for gravity or for sticking to a surface.
 var gravity_point: Vector2 = Vector2.ZERO
 
 # Direction that the segment will try to point towards (for standing on a surface)
 var stand_angle: float = NAN
+# When the last time that the segement was touching a surface.
+var last_stand: float = 0.0
 # Optional connections to other segments.
 var front_segment: WormSegment = null
 var segment_spacing: float = 30.0
@@ -27,19 +26,27 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if state.get_contact_count() > 0:
 		stand_angle = state.get_contact_local_normal(0).angle() + PI/2
 		gravity_point = global_position - state.get_contact_local_normal(0)*50
-	else:
+		last_stand = Time.get_ticks_msec()
+	elif Time.get_ticks_msec() - last_stand > 100:
 		stand_angle = NAN
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var gp: Vector2 = gravity_point
 	# Apply a torque to align the body segment with a surface.
+	# If not on a surface, align with front segment.
+	var target_angle: float = $AnimatedSprite2D.global_rotation
 	if is_finite(stand_angle):
-		var angle_diff: float = stand_angle - rotation
-		if angle_diff < -PI: angle_diff += 2*PI
-		if angle_diff > PI: angle_diff -= 2*PI
-		#TODO: smooth rotation
-		$AnimatedSprite2D.global_rotation = stand_angle
+		target_angle = stand_angle
+	elif front_segment != null:
+		target_angle = front_segment.get_node("AnimatedSprite2D").global_rotation
+	var angle_diff: float = target_angle - $AnimatedSprite2D.global_rotation
+	while angle_diff < -PI: angle_diff += 2*PI
+	while angle_diff > PI: angle_diff -= 2*PI
+	if abs(angle_diff) / delta <= 10.0:
+		$AnimatedSprite2D.global_rotation = target_angle
+	else:
+		$AnimatedSprite2D.global_rotation += 10.0 * delta * sign(angle_diff)
 	# Apply a force to bring the segment to the correct distance to the front and back neighbours.
 	# TODO: revisit this for when the worm is turning around.
 	if front_segment != null:
