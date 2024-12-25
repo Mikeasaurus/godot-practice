eec81a148322b5467d3c81c3a2bda54e3c8356fc
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2024-12-25 02:00:01 -0500

    Introduced a bug into the project.
    Also, started work on targeting for slime.

---
+++ slimetime/beetle.gd
@@ -0,0 +1,53 @@
+extends RigidBody2D
+
+# Initial velocity of bug
+var starting_velocity: float = 200
+# Springiness factor
+var springiness: float = 1
+
+# Original position of bug.
+var starting_position: Vector2
+
+# Whether bug is flying or inactive.
+var is_flying: bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AnimatedSprite2D.play()
+	starting_position = global_position
+	apply_central_impulse(Vector2(starting_velocity,0))
+	is_flying = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not is_flying:
+		apply_central_force(Vector2(0,Globals.gravity))
+		return
+	# For debugging - force bug to be in "slimed" state.
+	if Input.is_action_pressed("slime_all"):
+		get_slimed()
+	# Move the beetle
+	var dx: float = global_position.x - starting_position.x
+	var f: float = -springiness * dx
+	apply_central_force(Vector2(f,0))
+	# Seems to be some dampening, so keep pushing it.
+	if abs(dx) < 5:
+		if linear_velocity.x > 0:
+			apply_central_impulse(Vector2(starting_velocity-linear_velocity.x,0))
+		else:
+			apply_central_impulse(Vector2(-starting_velocity-linear_velocity.x,0))
+	# Angle it in direction of motion.
+	var angle: float
+	if linear_velocity.x > 0:
+		angle = -60.0
+	else:
+		angle = 60.0
+	# Smooth transition of angle near extremes of path.
+	if abs(linear_velocity.x) <= 20:
+		angle *= abs(linear_velocity.x)/20
+	$AnimatedSprite2D.global_rotation_degrees = angle
+
+# Change state of the bug so it's in a dormant, slimed state.
+func get_slimed () -> void:
+	is_flying = false
+	$AnimatedSprite2D.play("slimed")
--- slimetime/worm_front.gd
+++ slimetime/worm_front.gd
@@ -8,6 +8,9 @@ func flip_segment():
 	mouth_position.x *= -1
 	super()
 
+# Keep track of all bugs within range.
+var targets: Array = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Get position of mouth (for where to shoot slime from)
@@ -17,10 +20,26 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	# Check if we need to shoot some slime.
 	if Input.is_action_just_pressed("shoot_slime"):
+		# How fast slime shoots out.
+		var slime_speed: float = 1000.0
+		# Where the slime originates from (based on position of worm's mouth).
+		var slime_start: Vector2 = mouth_position.rotated($AnimatedSprite2D.rotation)
+		# Get the direction to shoot the slime.
+		# Check if any targets in range.
+		# Otherwise, shoot straight ahead.
+		var slime_direction: Vector2 = facing_direction
+		if len(targets) > 0:
+			slime_direction = (targets[0].global_position - (global_position+slime_start)).normalized()
 		var slime = slime_scene.instantiate()
-		slime.position = mouth_position.rotated($AnimatedSprite2D.rotation)
-		slime.linear_velocity = facing_direction * 1000.0
+		slime.position = slime_start
+		slime.linear_velocity = slime_direction * slime_speed
 		add_child(slime)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func __process(delta: float) -> void:
 	pass
+
+func _on_shooting_range_body_entered(body: Node2D) -> void:
+	targets.append(body)
+
+func _on_shooting_range_body_exited(body: Node2D) -> void:
+	targets.erase(body)
