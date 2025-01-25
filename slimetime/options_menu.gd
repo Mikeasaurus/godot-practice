88ca1baa037ad4f851d4182333a3298c4a64cc93
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-01-25 16:36:03 -0500

    Fix up the worm preview box for the options menu.
    Need to adjust the camera to centre the worm properly.

--- slimetime/options_menu.gd
+++ slimetime/options_menu.gd
@@ -8,6 +8,9 @@ func _ready() -> void:
 	$MarginContainer/CenterContainer/VBoxContainer/GridContainer/VolumeHSlider.value = Globals.volume
 	$MarginContainer/CenterContainer/VBoxContainer/GridContainer/VolumeHSlider.value_changed.connect(Globals.set_volume)
 	$MarginContainer/CenterContainer/VBoxContainer/GridContainer/GraphicsOptionButton.selected = Globals.graphics_level
+	# Align camera for worm preview.
+	# By default it's centered on the front torso, need to center on actual middle of worm.
+	$MarginContainer/CenterContainer/VBoxContainer/GridContainer/VBoxContainer/SubViewPortContainer/SubViewport/Worm/WormFront/AnimatedSprite2D/Camera2D.offset = Vector2(-32,-25)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
