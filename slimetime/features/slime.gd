29ceacc3e85672a7d42c085b037c30478b1deb09
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-04-13 14:47:47 -0400

    WIP: Moving splatter effect to its own scene.
    Note: This breaks splatter effect in multiplayer games.  Need to add some
    additional code in Screen to manage the effects in that context.

--- slimetime/features/slime.gd
+++ slimetime/features/slime.gd
@@ -1,7 +1,5 @@
 extends RigidBody2D
 
-@export var particle_scene: PackedScene
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -12,21 +10,6 @@ func _process(_delta: float) -> void:
 	# Apply gravity force.
 	apply_central_force(Vector2(0,Globals.gravity))
 
-# Spawn some splatter particles based around the given direction.
-@rpc("authority","call_local","reliable")
-func _create_splatter (pos: Vector2, direction: Vector2) -> void:
-	for i in range(10):
-		var p: Node2D = particle_scene.instantiate()
-		var angle: float = (randf() - 0.5) * PI
-		var speed: float = 100 + randf() * 200
-		p.position = pos
-		p.linear_velocity = direction.rotated(angle) * speed
-		p.z_index = z_index
-		# Have to do a deferred call for adding the particles, otherwise get the error message:
-		# ERROR: Can't change this state while flushing queries. Use call_deferred() or set_deferred() to change monitoring state instead.
-		call_deferred("add_sibling",p)
-	# Play a sound.
-	$SplatSound.play()
 
 # Get normal to any surface that's contacted, align direction vectors accordingly.
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
@@ -35,17 +18,10 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	# Only splat if a surface is hit, and only splat once.
 	if state.get_contact_count() > 0 and $Sprite2D.visible:
 		var n: Vector2 = state.get_contact_local_normal(0)
-		_create_splatter.rpc (position, n)
-		# Turn slime invisible until gets cleaned up.
-		$Sprite2D.visible = false
-		# Also stop it from colliding, so bugs don't get hit by stray, invisible slimes.
-		collision_layer = 0
-
-func _on_splat_sound_finished() -> void:
-	# Only clean up from server or single player.
-	# Remote clients will have the scene managed by a MultiplayerSpawner.
-	if multiplayer.get_unique_id() == 1:
+		var splatter: Splatter = Splatter.create(global_position, n)
+		add_sibling(splatter)
 		queue_free()
+
 
 # Clean up slime if it's been around for a long time and still hasn't hit anything.
 # Probably went out of bounds.
---
+++ slimetime/features/splatter.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+# Renders a slime splatter effect on the screen.
+class_name Splatter
+
+@export var particle_scene: PackedScene
+
+var direction: Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for i in range(10):
+		var p: Node2D = particle_scene.instantiate()
+		var angle: float = (randf() - 0.5) * PI
+		var speed: float = 100 + randf() * 200
+		p.position = position
+		p.linear_velocity = direction.rotated(angle) * speed
+		p.z_index = z_index
+		# Have to do a deferred call for adding the particles, otherwise get the error message:
+		# ERROR: Can't change this state while flushing queries. Use call_deferred() or set_deferred() to change monitoring state instead.
+		call_deferred("add_sibling",p)
+	# Play a sound.
+	$SplatSound.play()
+static func create (pos: Vector2, dir: Vector2) -> Splatter:
+	var splatter: Splatter = load("res://features/splatter.tscn").instantiate()
+	splatter.global_position = pos
+	splatter.direction = dir
+	return splatter
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_splat_sound_finished() -> void:
+	# Only clean up from server or single player.
+	# Remote clients will have the scene managed by a MultiplayerSpawner.
+	if multiplayer.get_unique_id() == 1:
+		queue_free()
