e5f1ecd944ff8fb2de98c5a4e40c0ceeda525194
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-04-29 19:49:31 -0400

    Some miscellaneous updates.
    - Mention rcedit for Windows builds.  I have it set up in my local directory,
      but other people would need to add it themselves to get an icon for the
      executable.
    - Add the Godot import file for the Nautica font.
    - Remove _process method for the download_menu script.

--- slimetime/menus/download_menu.gd
+++ slimetime/menus/download_menu.gd
@@ -7,10 +7,5 @@ func _ready() -> void:
 	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/WindowsLinkButton.uri = "./slimetime-v%s.zip"%Globals.version
 	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/LinuxLinkButton.uri = "./slimetime-linux-v%s.zip"%Globals.version
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_back_button_pressed() -> void:
 	MenuHandler.deactivate_menu()
