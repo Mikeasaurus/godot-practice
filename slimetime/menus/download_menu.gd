5220287f7e5546a3ccc87af23f9a6c2bcfe3c8e5
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-05-03 00:41:11 -0400

    Use unzipped version of executables for download link.
    Makes it easier to run it after downloading, at the expense of extra space on
    github pages.

--- slimetime/menus/download_menu.gd
+++ slimetime/menus/download_menu.gd
@@ -4,8 +4,8 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Add URI to current version.
-	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/WindowsLinkButton.uri = "./slimetime-v%s.zip"%Globals.version
-	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/LinuxLinkButton.uri = "./slimetime-linux-v%s.zip"%Globals.version
+	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/WindowsLinkButton.uri = "./slimetime-v%s.exe"%Globals.version
+	$ColorRect/MarginContainer/CenterContainer/VBoxContainer/HBoxContainer/LinuxLinkButton.uri = "./slimetime-v%s.x86_64"%Globals.version
 
 func _on_back_button_pressed() -> void:
 	MenuHandler.deactivate_menu()
