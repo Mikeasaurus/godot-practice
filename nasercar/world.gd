067dd814b8b3fc93ea7948e52765b31cc3f9a119
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-06-19 14:31:46 -0400

    Work on animation for Mango sliming the screen.
    Added a keyboard trigger to test it out.

--- nasercar/world.gd
+++ nasercar/world.gd
@@ -54,6 +54,8 @@ func _itemblock (car: Node2D) -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("use_item") and _has_item:
 		_use_item()
+	if event.is_action_pressed("slime_test"):
+		_slime_test()
 
 func _get_item() -> void:
 	if _has_item: return
@@ -77,3 +79,45 @@ func _use_item() -> void:
 	$ItemSelect.hide()
 	$ItemSelect/CenterContainer.scale = Vector2(0.5,0.5)
 	$ItemSelect/CenterContainer.modulate = Color.WHITE
+
+func _slime_test() -> void:
+	$ScreenEffects/MangoSlime.show()
+	var mango: AnimatedSprite2D = $ScreenEffects/MangoSlime/Mango
+	mango.offset.y = 0
+	mango.scale.y = 9
+	var mangotween: Tween = create_tween()
+	mangotween.set_ease(Tween.EASE_IN)
+	mangotween.tween_property(mango, "offset", Vector2(0,-108), 0.5)
+	mangotween.tween_interval(0.2)
+	#tween.tween_property(mango, "scale", Vector2(9,10), 0.0)
+	mangotween.tween_interval(0.4)
+	#tween.tween_property(mango, "scale", Vector2(9,9), 0.0)
+	mangotween.tween_interval(0.2)
+	mangotween.set_ease(Tween.EASE_OUT)
+	mangotween.tween_property(mango, "offset", Vector2(0,0), 0.3)
+	await get_tree().create_timer(0.7).timeout
+	mango.animation = "mouth_open"
+	$ScreenEffects/MangoSlime/CPUParticles2D.emitting = true
+	$ScreenEffects/MangoSlime/HorkSound.play()
+	await get_tree().create_timer(0.4).timeout
+	$ScreenEffects/MangoSlime/SplatSound.play()
+	var slime: AnimatedSprite2D = $ScreenEffects/MangoSlime/Slime
+	slime.show()
+	slime.play()
+	mango.animation = "default"
+	#await mangotween.finished
+	#slime.hide()
+	var slimedrip: Tween = create_tween()
+	slimedrip.set_ease(Tween.EASE_OUT)
+	var slimefade: Tween = create_tween()
+	slimefade.set_ease(Tween.EASE_OUT)
+	slimedrip.tween_property(slime, "scale", Vector2(10,50), 6.0)
+	slimefade.tween_interval(4.0)
+	slimefade.tween_property(slime, "modulate", Color.hex(0xffffff00), 2.0)
+	await slimefade.finished
+	if slimedrip.is_running():
+		await slimedrip.finished
+	slime.hide()
+	slime.scale.y = 10
+	slime.modulate = Color.WHITE
+	$ScreenEffects/MangoSlime.hide()
