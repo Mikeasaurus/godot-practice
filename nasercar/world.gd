3ac406b50fa1274fc9299a1329fdeed8d06e7ee7
Mikeasaurus <161517199+Mikeasaurus@users.noreply.github.com>
2025-06-21 00:30:09 -0400

    Implement the item selection logic.
    Relative probabilities of items depend on relative place of car in the race.
    
    Note: only slime item is actually hooked up, and CPU karts can't use it yet.

--- nasercar/world.gd
+++ nasercar/world.gd
@@ -29,6 +29,14 @@ func _cars_in_front_of (car: Car) -> Array[Car]:
 		if other_car._pathfollow.progress > car._pathfollow.progress:
 			cars.append(other_car)
 	return cars
+# Helper method - get number of cars behind the specified car.
+func _num_cars_behind (car: Car) -> int:
+	var num: int = 0
+	for other_car in _cars():
+		if other_car._pathfollow.progress < car._pathfollow.progress:
+			num += 1
+	return num
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -71,10 +79,14 @@ func _process(_delta: float) -> void:
 	pass
 
 # All available item types.
-enum ItemType {NONE,SLIME}
+enum ItemType {NONE,NAILPOLISH,COFFEE,METEOR,SLIME,BEETLE}
 # Map the item types to the visual representations.
 @onready var item_pics: Dictionary = {
+	ItemType.NAILPOLISH: $ItemSelect/CenterContainer/ScrollContainer/VBoxContainer/NailPolish,
+	ItemType.COFFEE: $ItemSelect/CenterContainer/ScrollContainer/VBoxContainer/Coffee,
+	ItemType.METEOR: $ItemSelect/CenterContainer/ScrollContainer/VBoxContainer/Meteor,
 	ItemType.SLIME: $ItemSelect/CenterContainer/ScrollContainer/VBoxContainer/MangoSlime,
+	ItemType.BEETLE: $ItemSelect/CenterContainer/ScrollContainer/VBoxContainer/Beetle,
 }
 
 func _itemblock (car: Car) -> void:
@@ -92,11 +104,53 @@ func _input(event: InputEvent) -> void:
 
 func _get_item(car) -> void:
 	# First, select an item.
-	# Put stub entry for this car, to prevent getting another item.
+	# Put stub entry for this car, to prevent getting another item while this
+	# item is arriving.
 	_current_items[car] = ItemType.NONE
-	var item: ItemType = ItemType.SLIME
-	#TODO: remove this hard-coded selection of Mango slime.
-	_sliming = true
+	var cars_in_front: Array[Car] = _cars_in_front_of(car)
+	# Determine which items are viable selections, and relative probability
+	# of occurrence.
+	var likelihood: Dictionary = {}
+	# Probability of slime increases with number of cars in front.
+	# But not available if someone else already has it / is using it.
+	if not _sliming:
+		if len(cars_in_front) > 0:
+			likelihood[ItemType.SLIME] = len(cars_in_front)*10
+	# Coffee also becomes more likely the further behind (at least a few cars behind?)
+	if len(cars_in_front) >= 3:
+		likelihood[ItemType.COFFEE] = len(cars_in_front)*10
+	# Similarly, probability of meteor increases with number of cars in front.
+	# But relatively less likely overall.
+	if not _meteor:
+		if len(cars_in_front) > 0:
+			likelihood[ItemType.METEOR] = len(cars_in_front)
+	# Probability of nail polish increases with number of cars behind.
+	var num_cars_behind: int = _num_cars_behind(car)
+	if num_cars_behind > 0:
+		likelihood[ItemType.NAILPOLISH] = num_cars_behind*10
+	# Beetle has equal probability, except for leading car.
+	if len(cars_in_front) > 0:
+		likelihood[ItemType.BEETLE] = 40
+	# Pick an item from these possibilities.
+	var sample_size: int = 0
+	for n in likelihood.values():
+		sample_size += n
+	var choice: int = randi() % sample_size
+	#print ('?? ', car, ' ', likelihood, ' ', sample_size)
+	var item: ItemType
+	for item_type in likelihood.keys():
+		if choice < likelihood[item_type]:
+			item = item_type
+			break
+		choice -= likelihood[item_type]
+	# An item should now be selected.
+
+	# Some item types are exclusive (only one available at a time).
+	if item == ItemType.METEOR:
+		_meteor = true
+	if item == ItemType.SLIME:
+		_sliming = true
+
 	# If this is player, then spin to this item.
 	if car.type == car.CarType.PLAYER:
 		# Move item picture to the last entry.
